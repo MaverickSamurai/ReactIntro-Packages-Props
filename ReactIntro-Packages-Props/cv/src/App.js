896016f0fd832500bccde39e40d33cb17985953f
MaverickSamurai <mirsamirba@code.edu.az>
2022-11-22 05:41:49 +0400

    ReactIntro-Packages-Props

---
+++ ReactIntro-Packages-Props/cv/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as React from 'react';
+import './App.css';
+import Cv from './components/layout/Cv';
+import Home from './pages/Home';
+
+
+
+
+function App() {
+
+  return (
+
+    <div>
+      <Home />
+      <Cv/>
+    </div>
+
+
+  );
+}
+
+export default App;
---
+++ ReactIntro-Packages-Props/cv/src/components/layout/Cv.js
@@ -0,0 +1,161 @@
+import React from 'react'
+
+function Cv() {
+  return (
+    <div>
+        <div className='container'>
+           
+<div id="doc2" class="yui-t7">
+	<div id="inner">
+	
+		<div id="hd">
+			<div class="yui-gc">
+				<div class="yui-u first">
+					<h1>Mirsamir Azimzada</h1>
+					<h2>Fullstack Developer</h2>
+				</div>
+
+				<div class="yui-u">
+					<div class="contact-info">
+						<h3><a id="pdf" href="#">Download PDF</a></h3>
+						<h3><a href="mailto:name@yourdomain.com">mirsamirba@code.edu.az</a></h3>
+						<h3>(+994)505704590</h3>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="bd">
+			<div id="yui-main">
+				<div class="yui-b">
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Profile</h2>
+						</div>
+						<div class="yui-u">
+							<p class="enlarge">
+							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+							</p>
+						</div>
+					</div>
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Skills</h2>
+						</div>
+						<div class="yui-u">
+
+								<div class="talent">
+									<h2>\C#</h2>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+								</div>
+
+								<div class="talent">
+									<h2>HTML</h2>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.	 </p>
+								</div>
+
+								<div class="talent">
+									<h2>Project Javascript</h2>
+									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+								</div>
+						</div>
+					</div>
+
+					<div class="yui-gf">
+						<div class="yui-u first">
+							<h2>Technical</h2>
+						</div>
+						<div class="yui-u">
+							<ul class="talent">
+								<li>XHTML</li>
+								<li>CSS</li>
+								<li class="last">Javascript</li>
+							</ul>
+
+							<ul class="talent">
+								<li>Jquery</li>
+								<li>PHP</li>
+								<li class="last">CVS / Subversion</li>
+							</ul>
+
+							<ul class="talent">
+								<li>OS X</li>
+								<li>Windows XP/Vista</li>
+								<li class="last">Linux</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="yui-gf">
+	
+						<div class="yui-u first">
+							<h2>Experience</h2>
+						</div>
+
+						<div class="yui-u">
+
+							<div class="job">
+								<h2>Facebook</h2>
+								<h3>Junior Developer</h3>
+								<h4>2017-2020</h4>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry </p>
+							</div>
+
+							<div class="job">
+								<h2>Apple Inc.</h2>
+								<h3>Middle Developer</h3>
+								<h4>2020-2022</h4>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry </p>
+							</div>
+
+							<div class="job">
+								<h2>Microsoft</h2>
+								<h3>Senior Developer</h3>
+								<h4>2022-2023</h4>
+								<p>Intrinsicly transform flexible manufactured products without excellent intellectual capital. Energistically evisculate orthogonal architectures through covalent action items. Assertively incentivize sticky platforms without synergistic materials. </p>
+							</div>
+
+
+							<div class="job last">
+								<h2>International Business Machines (IBM)</h2>
+								<h3>Senior Developer</h3>
+								<h4>2023-2027</h4>
+								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+							</div>
+
+						</div>
+					</div>
+
+
+					<div class="yui-gf last">
+						<div class="yui-u first">
+							<h2>Education</h2>
+						</div>
+						<div class="yui-u">
+							<h2>Code-Academy</h2>
+							<h3>Dual Major, Economics and English &mdash; <strong>4.0 GPA</strong> </h3>
+						</div>
+					</div>
+
+
+				</div>
+			</div>
+		</div>
+
+		<div id="ft">
+			<p>Mirsamir Azimzada &mdash; <a href="mirsamirba@code.edu.az">mirsamirba@code.edu.az</a> &mdash; (+994)505704590</p>
+		</div>
+
+	</div>
+
+
+</div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Cv
